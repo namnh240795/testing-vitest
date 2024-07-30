3193af735a06b329dc96dd14a6dbccd1521d256f
Nam Nguyen <namnh240795@gmail.com>
2024-07-30 11:02:41 +0700

    chore: add test hooks normal case

---
+++ vite-project/src/hooks/useCounter.test.ts
@@ -0,0 +1,17 @@
+// write test for useCounter.ts
+
+import { expect, it, describe } from "vitest";
+import { renderHook, act } from '@testing-library/react-hooks';
+import useCounter from './useCounter';
+
+describe('should increment counter', () => {
+    it('should return a default search term and original items', () => { 
+        const { result } = renderHook(() => useCounter());
+        const [count, setCount] = result.current;
+        expect(count).toBe(0);
+        act(() => {
+            setCount(1);
+        })
+        expect(result.current[0]).toBe(1);
+    });
+});
---
+++ vite-project/src/hooks/useCounter.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Counter = () => {
+    return React.useState(0);
+}
+
+export default Counter;
